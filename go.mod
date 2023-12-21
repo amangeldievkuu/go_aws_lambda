c1cd3408854aee6d0a5c2e1a1d33b08d4ccd2590
amangeldievkuu <amangeldievkuu@gmail.com>
2023-12-21 23:14:10 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module awsLambdaFunction
+
+go 1.21
+
+require github.com/aws/aws-lambda-go v1.43.0
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Request struct {
+	ID    float64 `json:"id"`
+	Value string  `json:"value"`
+}
+
+type Response struct {
+	Message string `json:"message"`
+	OK      bool   `json:"ok"`
+}
+
+func Handler(request Request) (Response, error) {
+	return Response{
+		Message: fmt.Sprintf("Process Request ID %f", request.ID),
+		OK:      true,
+	}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
